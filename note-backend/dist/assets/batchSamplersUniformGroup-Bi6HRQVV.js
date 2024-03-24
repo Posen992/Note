442e4c355349f7d6c100f7e8fb855a8ce418a9df
Posen992 <chen.Posen992@gmail.com>
2024-03-24 23:00:55 +0200

    upload the dist

---
+++ note-backend/dist/assets/batchSamplersUniformGroup-Bi6HRQVV.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-CzS_PEoc.js";import{U as o}from"./colorToUniform-DHg1N2LQ.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
